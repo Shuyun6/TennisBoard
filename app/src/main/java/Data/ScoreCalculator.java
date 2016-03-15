252ee16ca442a916b3c2e3cb283d2457f2336e51
Shuyun6 <shuyungogogo@hotmail.com>
2016-03-15 22:34:20 +0800

    commit

---
+++ app/src/main/java/Data/ScoreCalculator.java
@@ -0,0 +1,28 @@
+package Data;
+
+/**
+ * Created by Shuyun on 3/13/2016/013.
+ */
+public class ScoreCalculator {
+
+    private int score[]=new int[2];
+
+    public ScoreCalculator(){
+        this.score[0]=0;
+        this.score[1]=0;
+    }
+
+    private void calculate(){
+
+    }
+
+    public void putData(){
+
+    }
+
+    public int[] getData(){
+
+        return score;
+    }
+
+}
